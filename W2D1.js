39b1d189c5cf1ef3cddd1f30c114bc0f2a147ecf
Instructor Melissa @Coding Dojo <mlongenberger@codingdojo.com>
2021-02-01 19:20:01 +0000

    saving solution

--- W2D1.js
+++ W2D1.js
@@ -2,7 +2,18 @@
 
 function sum_add_5000(){
     var sum = 0;
-    // Enter Code here
+    for (i = 1; i <= 5000; i++) { // i=1 means start at 1 i <=5000 go through all number up to and = 5000, i++ increment the value of i by 1 each time through the loop
+        if (i % 2 == 1) {  // if odd set it to sum and then go to next
+            sum = sum + i
+        }  
+    }
 
     return sum;
 }
+sum_add_5000()
+// for loop - go through the all numbers to accomplish the function
+// modulus - conditional statement like if 0 remainder = even 1 remainder = odd
+
+// 1
+// 1+ 3
+// 4+5
