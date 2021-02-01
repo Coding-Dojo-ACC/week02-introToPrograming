49ae59c7fea98de1aeb6d40028c8b8315d311f0b
Instructor Melissa @Coding Dojo <mlongenberger@codingdojo.com>
2021-02-01 05:56:06 +0000

    adjusting files

--- W2D1-Solution.js
+++ W2D1-Solution.js
@@ -1,8 +1,8 @@
 // Write a function that returns the sum of all the odd numbers from 1 to 5000. (e.g. 1+3+5+...+4997+4999)
 
-function sum_add_5000(
+function sum_add_5000(){
     var sum = 0;
     // Enter Code here
 
     return sum;
-)
+}
