6f2995cff63ff86b9ab874ce5159fb46606aa15d
Instructor Melissa @Coding Dojo <mlongenberger@codingdojo.com>
2021-02-02 01:51:01 +0000

    adding notes and solutions

--- W2D1-Solution.js
+++ W2D1-Solution.js
@@ -2,44 +2,61 @@
 
 function sum_add_5000(){
     var sum = 0;
-    for (i = 1; i <= 5000; i++) { // i=1 means start at 1 i <=5000 go through all number up to and = 5000, i++ increment the value of i by 1 each time through the loop
-        if (i % 2 == 1) {  // if odd set it to sum and then go to next
+    for (var i = 1; i <= 5000; i++) {
+        if (i % 2 == 1) {
             sum = sum + i
-        }  
+        }
     }
-
     return sum;
 }
+console.log(sum_add_5000())
 
-// for loop - go through the all numbers to accomplish the function
-// modulus - conditional statement like if 0 remainder = even 1 remainder = odd
-
+// give the function instructions
+// for loop = go through all the numbers to accomplish the function.
+// for loop parts = 1. i= set the starting point  2. when to stop   3. i++  meaning add 1 to last item
+// if statement = if i = odd number then add it to sum
+// sum all odd number from 1-5000
 // 1
-// 1+ 3
+// 1+3
 // 4+5
+
+
+
+
+
+
+
 
 // Given an array with multiple values, write a function that returns the maximum number in the array. (e.g. for [-3,3,5,7] max is 7)
 
 function findMax(arr) {
-    var i;
-    var max = -Infinity;
-    for (i = 0; i < arr.length; i++) {
-        if (arr[i] > max) {
+    var max = arr[0]
+    for (var i = 0; i < arr.length; i++) {
+        if (max < arr[i]) {
             max = arr[i]
+            console.log(max)
         }
     }
 
     return max;
+    
 }
+console.log(findMax([100,2,24,75,60,102]))
+
+// give instructions
+// 
 
 // Write a function that will swap the first and last values of any given array. The default minimum length of the array is 2. (e.g. [1,5,10,-2] will become [-2,5,10,1]).
 
 function swap(arr) {
-    var arrnew = arr.slice();
-    var temp = arrnew[0];
-    arrnew[0] = arrnew[arrnew.length-1];
-    arrnew[arrnew.length-1] = temp;
+    var arrNew = arr.slice(); // manipulate the array
+    var temp = arrNew[0]; // temp remove index 0
+    arrNew[0] = arrNew[arrNew.length-1]; // index 0 is now what is at the last index
+    arrNew[arrNew.length-1] = temp; // take what was last index and put in temp back in arrnew at index 0
 
-    return arrnew;
+    return arrNew;
 }
-swap([1,2,5,6])
+arr = [2,4,7,9]
+console.log(swap(arr))
+
+// return [9,4,7,2]
